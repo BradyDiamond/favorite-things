bd6ed954224b528b0a7d79a9e410e0fdcbb975fa
Brady Diamond <brdydmnd@gmail.com>
2022-03-28 11:27:45 -0700

    add second array and return of it.
    Co-authored-by: Will Pratt <willpratt78@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,11 @@ $(document).ready(function() {
 
   const array1 = [name, food, movie, book, song, drink]
   let array2 = []
-  array2.push(array1[0],array1[2],array1[1])
+  array2.push(array1[1],array1[0],array1[2])
   console.log(array2)
 
-  $("#output").text("");
-  $("#output").append(array2)
+  $("#list").append("<li>" + array2[0]  + "</li>")
+  $("#list").append("<li>" + array2[1]  + "</li>")
+  $("#list").append("<li>" + array2[2]  + "</li>")
   });
 });
