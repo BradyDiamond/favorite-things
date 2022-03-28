d09a0ec21c66da25903df573513d8d9c44f1b4ef
Brady Diamond <brdydmnd@gmail.com>
2022-03-28 10:25:54 -0700

    add array to compile result
    Co-authored-by: Will Pratt <willpratt78@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   $("form#ft-input").submit(function(event) {
   event.preventDefault();
-  console.log("#book")
+  const name = $("input#name").val();
+  const food = $("input#food").val()
+  const movie = $("input#movie").val();
+  const book = $("input#book").val();
+  const song = $("input#song").val();
+  const drink = $("input#drink").val();
+
+  let array = [name, food, movie, book, song, drink]
+  console.log(array)
+
   });
 });
